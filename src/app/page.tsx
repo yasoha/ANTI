ddc2569bb6ffecbd2fa2f7f963d1fc349a9dd8d4
yasoha <zakariamz114@gmail.com>
2026-04-27 18:00:56 +0100

    Modify SelectedProduct interface and import components
    Updated the SelectedProduct interface to change the type of price from number to string and added new component imports.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-
+import { useState } from "react";
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import ProductGrid from '../components/ProductGrid';
 import Testimonials from '../components/Testimonials';
 import FAQ from '../components/FAQ';
+import TrustSignals from '../components/TrustSignals';
+import CheckoutModal from '../components/CheckoutModal';
+import WhatsAppButton from '../components/WhatsAppButton';
+import Footer from '../components/Footer';
+
 interface SelectedProduct {
   id: number;
   name: string;
-  price: number;
-  originalPrice: number;
+  price: string;
 }
 
 export default function Home() {
